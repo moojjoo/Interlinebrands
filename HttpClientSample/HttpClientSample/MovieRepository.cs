48d1b9aa5aab84dcf1eb6464182b83c09b04cdc0
Dannelly, Robert <robertdannelly@gmail.com>
2018-10-01 17:28:20 -0400

    Final for the day

---
+++ HttpClientSample/HttpClientSample/MovieRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace HttpClientSample
+{
+    class MovieRepository
+    {
+        HttpClient client = new HttpClient();
+
+        public MovieRepository()
+        {
+            client.BaseAddress = new Uri("https://localhost:44309/");
+            client.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<List<Movie>> GetAsync(CancellationToken cancellationToken)
+        {
+            await Task.Delay(3000);
+
+            cancellationToken.ThrowIfCancellationRequested();
+
+            HttpResponseMessage response = await client.GetAsync("api/movieitem", cancellationToken);
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsAsync<List<Movie>>();
+            }
+            return new List<Movie>();
+
+        }
+        
+    }
+}
--- InterviewMvcApi/InterviewMvcApi/Models/MovieItem.cs
+++ InterviewMvcApi/InterviewMvcApi/Models/MovieItem.cs
@@ -14,6 +14,5 @@ namespace InterviewMvcApi.Models
         public string Genre { get; set; }
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
-
     }
 }

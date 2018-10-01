ac679db675adc5e07b06ff6cb40acfa135f2d251
Dannelly, Robert <robertdannelly@gmail.com>
2018-10-01 17:03:35 -0400

    Last Commit

--- HttpClientSample/HttpClientSample/Program.cs
+++ HttpClientSample/HttpClientSample/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace HttpClientSample
 {
     class Program
     {
-        static void Main(string[] args)
+        static HttpClient client = new HttpClient();
+
+        static void ShowMovie(Movie movie)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Name: {movie.Title}" +
+                $"{movie.Price}\tGenre: {movie.Genre}");
         }
+
+        static async Task<Uri> CreateMovieAsync(Movie movie)
+        {
+            HttpRequestMessage response = await client.PostAsJsonAsync(
+                "api/movieitem", movie);
+            response.EnsureSuccessStatusCode();
+            //return URI of the created resource
+            return response.Headers.Location;
+        }
+
+
     }
 }
